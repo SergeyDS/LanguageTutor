45e1ad35466b02a03935892a36712d527b83df2c
Sergey <sergey_1c@mail.ru>
2021-02-24 19:16:29 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LanguageTutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tutor = new Tutor();
+
+            while (true)
+            {
+                var word = tutor.GetRandomEngWord();
+                Console.WriteLine($"Как переводится слово: {word}?");
+                var userAnswer = Console.ReadLine();
+                if (tutor.CheckWord(word, userAnswer))
+                    Console.WriteLine("Правильно!");
+                else
+                {
+                    var correctAnswer = tutor.Translate(word);
+                    Console.WriteLine($"Неверно. Правильный ответ: {correctAnswer}");
+                }
+            }
+
+        }
+    }
+}
